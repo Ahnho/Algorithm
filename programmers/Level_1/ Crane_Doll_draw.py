357ee904192b12bb6b1d9474795fff264fcd6a7f
- <dksgh00@naver.com>
2020-12-14 13:41:10 +0000

    -_-

--- programmers/Level_1/ Crane_Doll_draw.py
+++ programmers/Level_1/ Crane_Doll_draw.py
@@ -1,19 +1,25 @@
+###########################################
+##           크레인 인형 뽑기              ##
+###########################################
+
 def solution(board, moves):
-    case = [0]
+    baseket = [0]
+    cnt = 0 
     for i in range(len(moves)):
         a = 0
         while 1:
             if a == len(board):
                 break
             if board[a][moves[i] - 1] != 0 :
-                case.append(board[a][moves[i] - 1])
+                baseket.append(board[a][moves[i] - 1])
                 board[a][moves[i] - 1] = 0
                 break 
             a += 1
-        if len(case) >= 2 :
-            if case[len(case)-1] == case[len(case)-2]:
-                case.pop()
-                case.pop()
+        if len(baseket) >= 2 :
+            if baseket[len(baseket)-1] == baseket[len(baseket)-2]:
+                baseket.pop()
+                baseket.pop()
+                cnt += 2
 
         
-    return len(case)
+    return cnt
---
+++ programmers/Level_1/Pick_two_and_add.py
@@ -0,0 +1,13 @@
+###########################################
+##             Pick two and add          ##
+###########################################
+
+def solution(numbers):
+    answer = []
+    
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            if i != j:
+                answer.append(numbers[i]+ numbers[j])
+
+    return sorted(list(set(answer)))
