7e2fb2a120043e94c1a2cc2f80420406030bcb76
- <dksgh00@naver.com>
2021-02-08 14:13:39 +0000

    o

---
+++ programmers/Level_2/Phone_number_list.py
@@ -0,0 +1,11 @@
+###########################################
+##           전화번호 목록                 ##
+###########################################
+
+def solution(phone_book):
+
+    for i in range(len(phone_book)):
+        for k in range(i+1,len(phone_book)):
+            if (phone_book[i] == (phone_book[k])[0:len(phone_book[i])]) or (phone_book[k] == (phone_book[i])[0:len(phone_book[k])]) :
+                return False
+    return True
