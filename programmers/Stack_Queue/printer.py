c8dec22acf43874514376d4e12414963298f05a2
root <dksgh00@naver.com>
2021-07-10 10:58:44 +0000

    printer

---
+++ programmers/Stack_Queue/printer.py
@@ -0,0 +1,24 @@
+##############
+#  프린터
+# https://programmers.co.kr/learn/courses/30/lessons/42587
+##############
+
+def solution(priorities, location):
+    answer = 0
+    Q = [[p,i] for i,p in enumerate(priorities)]
+    mx = max(priorities)
+    
+    while 1 : 
+        ft = Q.pop(0)
+        ftp = priorities.pop(0)
+        if mx == ft[0]:
+            answer += 1
+            if location == ft[1]:
+                break
+            mx = max(priorities)
+        else:
+            Q.append(ft)
+            priorities.append(ftp)
+        
+    
+    return answer
