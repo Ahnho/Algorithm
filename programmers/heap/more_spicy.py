bba66421758d4790960cdd343d459211cbd7459c
Ahnho <dksgh00@naver.com>
2021-07-14 23:01:43 +0900

    more_spicy

---
+++ programmers/heap/more_spicy.py
@@ -0,0 +1,19 @@
+##############
+#  더 맵게
+# https://programmers.co.kr/learn/courses/30/lessons/42626
+##############
+
+import heapq as hq 
+
+def solution(scoville, K):
+    answer = 0
+    hq.heapify(scoville)
+
+    while scoville[0] < K :
+        if len(scoville) < 2 :
+            return -1
+        a,b = hq.heappop(scoville),hq.heappop(scoville)
+        hq.heappush(scoville, a + b*2)
+        answer += 1
+        
+    return answer
