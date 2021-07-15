b2abc17c995dbb5940012dcd5c50d90091f24c41
root <dksgh00@naver.com>
2021-07-15 13:27:04 +0000

    hash

---
+++ programmers/Hash/ Camouflage.py
@@ -0,0 +1,13 @@
+##############
+#  위장
+# https://programmers.co.kr/learn/courses/30/lessons/42578
+##############
+
+def solution(clothes):
+    answer = 1
+    ty = [i[1] for i in clothes]
+    kind = set(ty) 
+    for ch in kind:
+        answer *= ty.count(ch)+1
+
+    return answer-1
