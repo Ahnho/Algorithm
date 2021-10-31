e22eaeb88d927daa754623efb7f4b7e151916737
ahnho <dksgh00@navercom>
2021-10-31 10:45:40 +0900

    Dynamic Programing

---
+++ LeetCode/Climbing_Stairs.py
@@ -0,0 +1,12 @@
+###################################################################
+#  Climbing Stairs
+###################################################################
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a,b = 0,1 
+        for i in range(n):
+            a,b = b, a+b
+        return b
+        
+        
