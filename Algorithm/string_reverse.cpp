e0dcf2edeece5fe90ff172415a978fb7c7e119f5
Ahnho <dksgh00@naver.com>
2022-09-28 16:38:11 +0900

    string reverse recursion

---
+++ Algorithm/string_reverse.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+string reverse(string &a, int h, int n);
+
+int main(){
+    int numTestCases;
+    cin >> numTestCases;
+    for (int i = 0; i < numTestCases; ++i){
+        string st;
+        cin >> st;
+        int n = st.length()-1;
+        int h = 0;
+        cout << reverse(st,h, n);
+        printf("\n");
+    }
+    return 0;
+}
+
+
+
+string reverse(string &a, int h ,int n){
+    char temp;
+    if(h < n){
+        temp = a[h];
+        a[h] = a[n];
+        a[n] = temp;
+        reverse(a,h+1,n-1);
+    }
+    return a;
+}
+
