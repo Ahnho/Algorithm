4d15e942cdb30d740e857b52e1e7304caa5a1d53
Ahnho <dksgh00@naver.com>
2023-03-27 10:41:37 +0900

    막대기 분힐

---
+++ Practice_project/Splitting_sticks.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+for _ in range(n):
+    lin = input()
+    stack = []
+    bars = 0
+    for i in range(len(lin)):
+        if lin[i] == '(' and lin[i+1] == '(':
+            stack.append(0)
+        elif lin[i] == ')' and lin[i-1] == ')':
+            bars += stack.pop() + 1
+        elif lin[i] == '(' and lin[i+1] == ')':
+            for j in range(len(stack)):
+                stack[j] += 1
+
+    print(bars)
+    
